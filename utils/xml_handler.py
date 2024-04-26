742a8656c5d8d98ab73d90615aa9c9b3dc0f21d6
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-04-26 16:55:25 +0200

    Remove mapping data to dictionary

--- utils/xml_handler.py
+++ utils/xml_handler.py
@@ -1,18 +1,6 @@
 import xml.etree.ElementTree as ElementTree
 
 
-def data_to_xml_dictionary(data):
-    xml_data = {
-        "FileSize": data['file_size'],
-        "FileExtension": data['file_extension'],
-        "ModificationDate": data['modification_date'],
-        "DocumentHash": data['document_hash'],
-        "UserInfo": data['user_info'],
-        "Timestamp": data['timestamp']
-    }
-    return xml_data
-
-
 def create_xml(tags):
     signature = ElementTree.Element("Signature")
     for tag, value in tags.items():

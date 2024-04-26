b8c6acc059f538e4a6bcf4296bed019487212441
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-04-26 17:37:51 +0200

    Add prettying xml string

--- utils/xml_handler.py
+++ utils/xml_handler.py
@@ -1,9 +1,11 @@
 import xml.etree.ElementTree as ElementTree
+from xml.dom import minidom
 
 
 def create_xml(tags):
-    signature = ElementTree.Element("Signature")
+    signature = ElementTree.Element('Signature')
     for tag, value in tags.items():
         ElementTree.SubElement(signature, tag).text = str(value)
     xml_str = ElementTree.tostring(signature, encoding='unicode', method='xml')
-    return xml_str
+    pretty_xml = minidom.parseString(xml_str).toprettyxml(indent='\t')
+    return pretty_xml
